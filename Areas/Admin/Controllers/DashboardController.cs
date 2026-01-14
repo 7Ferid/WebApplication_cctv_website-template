80dbcab5bb9ba8280c96dbe01b030faa62035542
7Ferid <faridgg-mpa101@code.edu.az>
2026-01-15 01:09:51 +0400

    Add project files.

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_cctv_website_template.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
